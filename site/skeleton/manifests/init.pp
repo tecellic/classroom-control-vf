cdd076cb00a2bc6398c13281d9e6d208d74e9b51
tecellic <tecelli.celik@borsaistanbul.com>
2016-02-23 12:57:24 +0200

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -8,7 +8,7 @@ class skeleton{
  group   => '0',    
  mode    => '0644',    
  owner   => '0',  
- source => 'puppet:///modules/skeleton/manifest/bashrc',
+ source => 'puppet:///modules/skeleton/files/bashrc',
  type    => 'file',
  }  
 }  
