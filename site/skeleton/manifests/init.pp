a149cd5c4d7fad814c689d7fe5ad64a7b790545e
tecellic <tecelli.celik@borsaistanbul.com>
2016-02-23 13:21:44 +0200

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,13 @@
 class skeleton{  
  file { '/etc/skel/':    
  ensure => 'directory',
+ mode   => '0755',
  }  
   
  file { '/etc/skel/.bashrc':    
  ensure  => 'present',    
- group   => 'root',    
+ group   => 'root',
+ mode    => '0777'
  owner   => 'root',  
  source => 'puppet://modules/site/skeleton/files/bashrc',
  }  
