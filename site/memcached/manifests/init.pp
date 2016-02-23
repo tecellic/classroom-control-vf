c48c497c66d91c67d39c9dcf6694f5a71e1852b1
tecellic <tecelli.celik@borsaistanbul.com>
2016-02-23 14:51:18 +0200

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,16 @@
+class memcached {
+package{'memcached':
+  ensure  => present,
+  }
+  file {'/etc/sysconfig/memcached':
+    ensure => 'file',
+    source => 'puppet:///modules/memcached/sysconfig/memcached',
+    require => Package['memcached'],
+    notify  => Service['memcached'],
+    }
+    
+    service {'memcached':
+      ensure => 'running',
+      subscribe => File['/etc/sysconfig/memcached'],
+      }
+}
