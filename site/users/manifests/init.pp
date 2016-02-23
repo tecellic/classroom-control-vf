24681ebd7b21d3213ce9612bfcde36a33d6d3632
tecellic <tecelli.celik@borsaistanbul.com>
2016-02-23 16:28:45 +0200

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,12 +1,7 @@
 class users{
 user { 'fundamentals':
   ensure           => 'present',  
-  gid              => '1001',  
   home             => '/home/fundamentals',  
-  password         => '!!',  
-  password_max_age => '99999',  
-  password_min_age => '0',  
   shell            => '/bin/bash',  
-  uid              => '2001',
 }
 }
